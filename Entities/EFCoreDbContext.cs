ebd68442486351ae3cf404c1645dcb644309b674
tomz re <tomz re>
2016-11-08 16:52:50 +0000

    ConfigForPostgreSql

--- Entities/EFCoreDbContext.cs
+++ Entities/EFCoreDbContext.cs
@@ -1,5 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace EFCore.Entities{
-    public class EFCoreDbContext : DbContext{
+namespace EFCore.Entities
+{
+    public class EFCoreDbContext : DbContext
+    {
+
+        public EFCoreDbContext ()
+        {
+          
         }
+            public DbSet<Post> Posts { get; set; }
+            public DbSet<Category> Categories { get; set; }
+            public DbSet<Comment> Comments { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFCore.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -29,6 +31,7 @@ namespace TestAPI
         {
             // Add framework services.
             services.AddMvc();
+            services.AddDbContext<EFCoreDbContext>(options => options.UseNpgsql(Configuration["Data:PostgreConnection:ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
